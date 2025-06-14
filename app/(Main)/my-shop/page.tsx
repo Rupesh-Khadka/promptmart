9a8535bb0050fb75154cdad9cce85d34a131d766
Rupesh Khadka <rupeshkhadka000@gmail.com>
2025-06-14 21:40:15 +0545

    fixed all

--- app/(Main)/my-shop/page.tsx
+++ app/(Main)/my-shop/page.tsx
@@ -1,7 +1,15 @@
-export default function MyShop() {
+import { getAllPrompt } from "@/app/action";
+import { requireUser } from "@/app/utils/requireUser";
+import { ShopDashboard } from "@/components/general/shop/ShopDashboard";
+
+export default async function MyShop() {
+  const sellerId: any = await requireUser();
+  // const ordersData = await getUserOrders({ sellerId: sellerId?.user.id });
+  // const promptsData = await getAllPrompt();
   return (
     <div>
-      <h1>Dashboadr</h1>
+      {/* <ShopDashboard ordersData={ordersData} promptsData={promptsData} /> */}
+      Dashboard
     </div>
   );
 }
